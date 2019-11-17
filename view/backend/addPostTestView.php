b80162a2d277df117eb9cfec95ba31012b8b057a
Walid <hlaliwalid@mail.com>
2019-11-17 18:04:15 +0100

    add post style added

--- view/backend/addPostTestView.php
+++ view/backend/addPostTestView.php
@@ -1,25 +1,28 @@
-<div class="form_wrapper">
-    <form action="" method="post">
-        <header>Ajouter un article</header>
-        <div class="inputs">
+<form action="" method="post" class="form_wrapper">
+    <div class="form_wrapper_title">Ajouter un article</div>
+    <div class="form_wrapper_inputs">
         <div class="form-group">
-        <label for="author">Auteur</label><br>
-        <input class="form-control" placeholder="Auteur" type="text" id="author" name="author" required oninvalid="this.setCustomValidity('Remplir le champ svp.')" oninput="setCustomValidity('')" />
-    </div>
-    <div class="form-group">
-                    <label for="title">Titre</label><br>
-                    <input class="form-control" placeholder="Titre" id="title" name="title" required oninvalid="this.setCustomValidity('Remplir le champ svp')" oninput="setCustomValidity('')" />
-                </div>
-                <div class="form-group">
-                    <button class="btn btn-sm btn-info" type="submit" name="selectImg">Choisir Image</button>
-                </div>
+            <input class="form-control" placeholder="Auteur" type="text" id="author" name="author" required oninvalid="this.setCustomValidity('Remplir le champ svp.')" oninput="setCustomValidity('')" />
         </div>
-                <div class="form-group">
-                    <label for="title">Texte</label><br />
-                    <textarea id="myTextArea" name="content"></textarea>
-                </div>
-                <div id="buttonBox" class="mt-4">
-                    <button class="btn btn-md btn-success" type="submit" name="save">Poster</button>
-                </div>
-    </form>
-</div>
+        <div class="form-group">
+            <input class="form-control" placeholder="Titre" id="title" name="title" required oninvalid="this.setCustomValidity('Remplir le champ svp')" oninput="setCustomValidity('')" />
+        </div>
+        <div class="form-group">
+            <label for="exampleFormControlSelect1">Sujet</label>
+            <select class="form-control" id="exampleFormControlSelect1">
+                <option>Articles</option>
+                <option>Annonces</option>
+                <option>Evénements</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <button class="btn btn-sm btn-info" type="submit" name="selectImg">Choisir Image</button>
+        </div>
+    </div>
+    <div class="form_wrapper_tiny form-group">
+        <textarea id="myTextArea" name="content"></textarea>
+    </div>
+    <div id="buttonBox" class="form_wrapper_foot">
+        <button class="btn btn-md btn-success ml-auto" type="submit" name="save">Poster</button>
+    </div>
+</form>
